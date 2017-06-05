df1ef85ba5914179695acaace942bcf7a8f94f1d
Jason Wang <jasonwang67@gmail.com>
2017-06-04 19:00:35 -0700

    mongoose connection auth

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const path = require('path');
 const chalk = require('chalk');
-const DATABASE_URI = require(path.join(__dirname, '../env')).DATABASE_URI;
+const ENV_VARIABLES = require(path.join(__dirname, '../env'));
+const DATABASE_URI = ENV_VARIABLES.DATABASE_URI;
+const DATABASE_USER = ENV_VARIABLES.DATABASE_USER;
+const DATABASE_PASSWORD = ENV_VARIABLES.DATABASE_USER;
+
+const options = {
+  db: { native_parser: true},
+  user: DATABASE_USER,
+  pass: DATABASE_PASSWORD
+};
 
 mongoose.Promise = require('bluebird');
 console.log(chalk.yellow('Opening connection to MongoDB', DATABASE_URI));
-module.exports = mongoose.connect(DATABASE_URI);
+module.exports = mongoose.connect(DATABASE_URI, options);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
