7ed49b43252e70b7c32279291517c2a77851ebe6
Jason Wang <jasonwang67@gmail.com>
2017-06-04 19:12:34 -0700

    fixed typo import

--- server/db/index.js
+++ server/db/index.js
@@ -4,15 +4,13 @@ const chalk = require('chalk');
 const ENV_VARIABLES = require(path.join(__dirname, '../env'));
 const DATABASE_URI = ENV_VARIABLES.DATABASE_URI;
 const DATABASE_USER = ENV_VARIABLES.DATABASE_USER;
-const DATABASE_PASSWORD = ENV_VARIABLES.DATABASE_USER;
+const DATABASE_PASSWORD = ENV_VARIABLES.DATABASE_PASSWORD;
 
 const options = {
   db: { native_parser: true},
   user: DATABASE_USER,
   pass: DATABASE_PASSWORD
 };
-
-console.log(DATABASE_USER, DATABASE_PASSWORD)
 
 mongoose.Promise = require('bluebird');
 console.log(chalk.yellow('Opening connection to MongoDB', DATABASE_URI));
