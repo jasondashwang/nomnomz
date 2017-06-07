a068eaf92dd95182bcd094bf7ad195bb9dc2d83c
Jason Wang <jasonwang67@gmail.com>
2017-06-06 23:03:32 -0700

    cleaned up old files

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  email: {type: String, required: true},
-  fullName: {type: String, required: true},
-  position: {type: String},
-  pwd: {type: String, required: true},
-  roles: [{ role: 'userAdminAnyDatabase', db: 'admin'}]
-});
+// const userSchema = new Schema({
+//   firstName: {type: String, required: true},
+//   lastName: {type: String, required: true},
+//   email: {type: String, required: true},
+//   fullName: {type: String, required: true},
+//   position: {type: String},
+//   pwd: {type: String, required: true},
+//   roles: [{ role: 'userAdminAnyDatabase', db: 'admin'}]
+// });
 
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
